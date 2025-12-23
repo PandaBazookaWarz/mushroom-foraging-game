ad567523c006d66ab2762eded4d96dc1faa5e375
John-Allan Barger <pandabazookawarz@gmail.com>
2025-12-22 16:04:13 -0800

    Added basic top down player movement.

---
+++ player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const ACCEL = 2.0
+
+var input: Vector2
+
+func get_input():
+	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	return input.normalized()
+
+func _process(delta: float) -> void:
+	var player_input = get_input()
+	
+	velocity = lerp(velocity, player_input * SPEED, delta * ACCEL)
+	move_and_slide()
